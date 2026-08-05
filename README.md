39be7fe0649e71dba57f740ceefe92bfc87456d8
GrsemNow <GrsemNow@gmail.com>
2026-08-06 01:58:24 +0300

    add: Self-atrention (one head)

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 ## 🧠 Current Status
 
 - [x] Bigram model - the simplest language model without any context.
-- [ ] Self-attention (in progress).
+- [x] Self-attention.
+- [ ] Multi-head (in progress).
 - [ ] Text generation.
 - [ ] Visualizations.
+
+"""
+30000 2.6476, 2.6640
+
+Мгозм!
+Рог ие внел- ичтн.
+Жше буой,
+С Ководрочт гою позалаши чишадежда я годужесковечи товстекрод кололярнаяножихахараюмньлодук Ба
+сел ду лсеждтенискленю
+О доеконавно, н бамимрогомных еникиха о жепо д
+"""
--- main.py
+++ main.py
@@ -20,7 +20,6 @@ def prepare_data(filepath, trans, splitter="---"):
     
     return data, chars, encode, decode
 
-
 def make_batch(data, block_size, batch_size):
     # fabric of batches
     
@@ -52,10 +51,12 @@ def make_bigram_model(voc_size):
     return logits_fn, loss_fn, embedding_weight
 
 
-def generate_step(logits_fn, idx, count, temperature=1.0):
+def generate_step(logits_fn, idx, count, block_size,temperature=1.0):
     sequence = idx
     for _ in range(count):
-        logits = logits_fn(sequence)[:, -1, :] / temperature
+        idx_cond = sequence[:, -block_size:] if sequence.shape[1] > block_size else sequence 
+
+        logits = logits_fn(idx_cond)[:, -1, :] / temperature
         probs = F.softmax(logits, dim=1)
         idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
         sequence = torch.cat((sequence, idx_next), dim=1) # (B, T+1)
@@ -113,7 +114,70 @@ def estimate_loss(logits_fn, loss_fn, train_batch, val_batch, eval_iters=100):
         loss_val += loss.item()
     
     return loss_train / eval_iters, loss_val / eval_iters
+
+
+def self_attention(x, W_q, W_k, W_v, head_size, tril_mask=True):
+    # ONE head of self attension
+    B, T, C = x.shape
+
+    q = x @ W_q # (B, T, head_size)
+    k = x @ W_k # (...)
+    v = x @ W_v # (...)
+
+    wei = q @ k.transpose(-2, -1)
+    wei = wei / (head_size ** 0.5)
+
+    if tril_mask:
+        tril = torch.tril(torch.ones(T, T))
+        wei = wei.masked_fill(tril == 0, float('-inf'))
+
+    wei = F.softmax(wei, dim=-1) # (B, T, T)
+    out = wei @ v 
+
+    return out
+
+
+def make_attention_model(voc_size, n_embd, block_size, head_size):
+    embedding_weight = torch.randn(voc_size, n_embd, requires_grad=True)
+    position_embedding_table = torch.randn(block_size, n_embd, requires_grad=True)
+
+    # weightes
+    W_q = torch.randn(n_embd, head_size, requires_grad=True)
+    W_k = torch.randn(n_embd, head_size, requires_grad=True)
+    W_v = torch.randn(n_embd, head_size, requires_grad=True)
     
+    # linear layer
+    lm_head_weight = torch.randn(head_size, voc_size, requires_grad=True)
+    lm_head_bias = torch.randn(voc_size, requires_grad=True)
+    
+    def logits_fn(idx):
+        # (B, T) -> (B, T, C) logits
+        B, T = idx.shape
+        tok_emb = F.embedding(idx, embedding_weight)
+        pos_emb = F.embedding(torch.arange(T), position_embedding_table)
+        x = (tok_emb + pos_emb)
+        
+        # self-attention
+        x = self_attention(x, W_q, W_k, W_v, head_size)
+
+        logits = x @ lm_head_weight + lm_head_bias
+        return logits
+
+    def loss_fn(logits, targets):
+        # (logits + targets) -> loss
+        B, T, C = logits.shape
+        return F.cross_entropy(logits.view(B*T, C), targets.view(B*T))
+        
+    params = {
+            'embedding_weight': embedding_weight,
+            'position_embedding_table': position_embedding_table,
+            'W_q': W_q,
+            'W_k': W_k,
+            'W_v': W_v,
+            'lm_head_weight': lm_head_weight,
+            'lm_head_bias': lm_head_bias,
+            }
+    return logits_fn, loss_fn, params
 
 if __name__ == "__main__":
     torch.manual_seed(239)
@@ -122,9 +186,10 @@ if __name__ == "__main__":
     block_size = 64
     batch_size = 32
     lr = 1e-3
-    count = 10000
+    count = 30000
     eval_iters = 200
-    n_embd = 32
+    n_embd = 64
+    head_size = 32
     
     # prepare data
     trans = make_translation_table()
@@ -135,8 +200,8 @@ if __name__ == "__main__":
     train_data, val_data = data[:n], data[n:]
     
     # model
-    logits_fn, loss_fn, weights = make_bigram_model(voc_size)
-    optimizer = torch.optim.AdamW([weights], lr=lr)
+    logits_fn, loss_fn, params = make_attention_model(voc_size, n_embd, block_size, head_size)
+    optimizer = torch.optim.AdamW(list(params.values()), lr=lr)
     
     # studing
     train_batch = make_batch(train_data, block_size, batch_size)
@@ -150,7 +215,7 @@ if __name__ == "__main__":
     
     # generation
     idx = torch.zeros((1,1), dtype=torch.long)
-    gen = generate_step(logits_fn, idx, count=100)
+    gen = generate_step(logits_fn, idx, count=200, block_size=block_size)
     for i, seq in enumerate(gen, 1):
         pass
     print(decode(seq[0].tolist()))
